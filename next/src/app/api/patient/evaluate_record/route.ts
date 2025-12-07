ab9951dbf029c023316a9d2a691b0c7342f9cc89
jc14ai <jestalycastillo15@gmail.com>
2025-12-07 20:11:20 +0800

    Refactor: gets result id instead of assign id

--- next/src/app/api/patient/evaluate_record/route.ts
+++ next/src/app/api/patient/evaluate_record/route.ts
@@ -3,44 +3,43 @@ import { NextResponse } from "next/server";
  
 export async function POST(req:Request){
     try {
-        const { exercise, assignId  } = await req.json();
+        const { exercise, resultId  } = await req.json();
         const res = await fetch(`http://127.0.0.1:5000/evaluate/${exercise}`);
 
         const data = await res.json();
         
-        const resultCreated = await prisma.result.create({
-            data:{
-                assign_id:Number(assignId),
-                score:Number(data.accuracy)
+        const resultUpdated = await prisma.result.update({
+            where:{
+                result_id:Number(resultId),
             },
-            select:{
-                result_id:true
+            data:{
+                score:Number(data.accuracy)
             }
         });
 
-        if(!resultCreated) return NextResponse.json({success:false, message:'Result not saved.'});
+        if(!resultUpdated) return NextResponse.json({success:false, message:'Result not saved.'});
 
-        const imageCreated = await prisma.result_image.createMany({
-            data:[
-                {
-                    result_id:resultCreated.result_id,
-                    image_name: 'image1',
-                    filepath: ''
-                },
-                {
-                    result_id:resultCreated.result_id,
-                    image_name: 'image2',
-                    filepath: ''
-                },
-                {
-                    result_id:resultCreated.result_id,
-                    image_name: 'image3',
-                    filepath: ''
-                },
-            ]
-        });
+        // const imageCreated = await prisma.result_image.createMany({
+        //     data:[
+        //         {
+        //             result_id:resultCreated.result_id,
+        //             image_name: 'image1',
+        //             filepath: ''
+        //         },
+        //         {
+        //             result_id:resultCreated.result_id,
+        //             image_name: 'image2',
+        //             filepath: ''
+        //         },
+        //         {
+        //             result_id:resultCreated.result_id,
+        //             image_name: 'image3',
+        //             filepath: ''
+        //         },
+        //     ]
+        // });
 
-        if (!imageCreated) return NextResponse.json({success:false, message:'Image not saved.'});
+        // if (!imageCreated) return NextResponse.json({success:false, message:'Image not saved.'});
 
         return NextResponse.json({success:true});
     } catch (error) {
